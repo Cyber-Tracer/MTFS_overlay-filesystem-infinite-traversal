7dcc08f4cd2c2a003deb94134fb870e616fcdbf2
Jan von der Assen <jan.vda@gmx.ch>
2023-03-07 16:02:59 +0100

    Create mount.sh

---
+++ cmd/infiniteDirectory/mount.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+mkdir -p $HOME/000/!
+mkdir -p $HOME/001/!
+mkdir -p $HOME/002/!
+mkdir -p $HOME/003/!
+mkdir -p $HOME/004/!
+mkdir -p $HOME/005/!
+mkdir -p $HOME/006/!
+mkdir -p $HOME/007/!
+mkdir -p $HOME/008/!
+mkdir -p $HOME/009/!
+mkdir -p $HOME/010/!
+mkdir -p $HOME/011/!
+mkdir -p $HOME/012/!
+mkdir -p $HOME/013/!
+mkdir -p $HOME/014/!
+mkdir -p $HOME/015/!
+mkdir -p $HOME/016/!
+mkdir -p $HOME/017/!
+mkdir -p $HOME/018/!
+mkdir -p $HOME/019/!
+mkdir -p $HOME/020/!
+
+
+
+go run main.go /000/!&
+go run main.go /001/!&
+go run main.go /002/!&
+go run main.go /003/!&
+go run main.go /004/!&
+go run main.go /005/!&
+go run main.go /006/!&
+go run main.go /007/!&
+go run main.go /008/!&
+go run main.go /009/!&
+go run main.go /010/!&
+go run main.go /011/!&
+go run main.go /012/!&
+go run main.go /013/!&
+go run main.go /014/!&
+go run main.go /015/!&
+go run main.go /016/!&
+go run main.go /017/!&
+go run main.go /018/!&
+go run main.go /019/!&
+go run main.go /020/!&
